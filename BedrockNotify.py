2537beaccba6be3b17f94bf41e37e4beb43481f4
Chris Tracy <58871574+ChrisTracy@users.noreply.github.com>
2022-01-17 12:35:53 -0600

    Adding Function and ALERT env

--- BedrockNotify.py
+++ BedrockNotify.py
@@ -1,68 +1,75 @@
-#import packages
 import os
 import glob
 import requests
 import time
 
-#Set variables from env
 webhook_url = os.environ['WEBHOOK']
 interval = os.environ['INTERVAL']
+alerts = os.environ['ALERTS']
+
+#convert interval to int
 interval = int(interval)
 
-#Start loop
+def LogNotifyInfo(var, type):
+    #Get last var
+    array_length = len(var)
+    last_var = var[array_length - 1]
+
+    #Check lastsent file to prevent double notif
+    last_sent = open(f'/app/last_sent_{type}.txt', 'r').read()
+
+    #Compare and send if they don't match
+    if last_var in last_sent:
+        print(f"New {type} log not detected")
+    else:
+        print(f"New {type} log detected")
+        #Write out to check next time
+        baw = open(f"/app/last_sent_{type}.txt", "w")
+        baw.write(last_var)
+        baw.close()
+
+        #Format nicely for Discord
+        split_string_front = last_var.split("INFO] ", 1)
+        substring_front = split_string_front[1]
+        split_string_back = substring_front.split(", ", 1)
+        DiscordMessage = split_string_back[0]
+        print(DiscordMessage)
+
+        #Send to Discord
+        Message = {
+            "content": DiscordMessage
+        }
+        requests.post(webhook_url, data=Message)
+
+
 while True:
 
     #init array
     connected = []
+    disconnected = []
 
     # Pull  log files
     list_of_files = glob.glob('/MClogs/*') 
     latest_file = max(list_of_files, key=os.path.getctime)
 
-    #Open last Log and Check for login
+    #Open last Log and Check for log
     file = open(latest_file, 'r')
     Lines = file.readlines()
 
-    #Loop lines and add connected players to array
+    #Loop lines and add log line to array
     for Line in Lines:
-        if "Player connected" in Line or "Player disconnected" in Line:
+        if "Player connected" in Line:
             connected.append(Line)
+        if "Player disconnected" in Line:
+            disconnected.append(Line)
 
-    #If connected contains information move on
-    if connected:
+    #Check params and send notice
+    if "connected" in alerts:
+        if connected:
+            LogNotifyInfo(var=connected, type="connected")
+    if "disconnected" in alerts:
+        if disconnected:
+            LogNotifyInfo(var=disconnected, type="disconnected")
 
-        #Get last connected
-        array_length = len(connected)
-        last_connected = connected[array_length - 1]
-
-        #Check lastsent file to prevent double notif
-        last_sent = open('/app/last_sent.txt', 'r').read()
-
-        #Compare and send if they don't match
-        if last_connected in last_sent:
-            print("No new login found in Logs")
-        else:
-            print("New Login detected in Logs")
-            #Write out to check next time
-            baw = open("/app/last_sent.txt", "w")
-            baw.write(last_connected)
-            baw.close()
-
-            #Format nicely for Discord
-            split_string_front = last_connected.split("INFO] ", 1)
-            substring_front = split_string_front[1]
-            split_string_back = substring_front.split(", ", 1)
-            DiscordMessage = split_string_back[0]
-            print(DiscordMessage)
-
-            #Send to Discord
-            Message = {
-                "content": DiscordMessage
-            }
-            requests.post(webhook_url, data=Message)
-        #clear variables
-        connected = ""
-        last_connected = ""
-
-    #sleep while loop
+    #sleep for loop
     time.sleep(interval)
